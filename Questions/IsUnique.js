0d572b833c318eece6ca181559bb5c975cf65cf2
Hailong Yang <yhlong2@gmail.com>
2018-10-14 23:30:52 -0500

    Create Unique JS
    Create Unique JS

---
+++ Questions/IsUnique.js
@@ -0,0 +1,25 @@
+/*
+  Is Unique: Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
+  
+*/
+
+
+```
+function UniqueString(str) {
+  if(str.length > 128) {
+    return false;
+  }
+  
+  let charObjs = {};
+  
+  for(let i = 0; i < str.length; i++) {
+    if(charObjs[str.charAt(i)]) {
+      return false;
+    }
+    charObjs[str.charAt(i)] = true    
+  }
+  
+  return true;
+}
+
+```
