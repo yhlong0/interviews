9252c4d91e1bf9a94403e2d0c01afc7e36e7f28c
yhlong0 <yhlong2@gmail.com>
2018-10-29 22:46:46 -0500

    add RotateMatrix

--- Questions/RotateMatrix.js
+++ Questions/RotateMatrix.js
@@ -2,3 +2,25 @@
 *Rotate Matrix: Given an image represented by an NxN matrix, where each pixel in the image is 4
 *bytes, write a method to rotate the image by 90 degrees. Can you do this in place?
 */
+
+function RotateMatrix(matrix) {
+
+
+    return matrix
+}
+
+
+let m1 = [
+    [1, 2],
+    [3, 4]
+];
+
+
+let m2 = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+
+
+console.log(RotateMatrix(m1));
